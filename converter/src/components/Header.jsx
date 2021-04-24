4e20d840b7e23b9485450d8dbb27b4fe1b4d36ac
Alina <goncharenko0206@gmail.com>
2021-04-24 22:32:14 +0300

    правки

--- converter/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-
-    return (
-        <div>
-            <h1 className="title">Конвертер української гривні в іноземну валюту<br />(курс Національного банку України)</h1>
-        </div>
-    )
-}
